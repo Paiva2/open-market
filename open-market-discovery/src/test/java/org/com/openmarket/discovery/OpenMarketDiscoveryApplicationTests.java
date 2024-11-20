e6d33e11f5fcfab09e0a9ab03dcefe24e78dcb01
paiva2 <joaovitor.paiva145@hotmail.com>
2024-11-20 14:40:12 -0300

    [CHORE] init project and start Discovery Server

---
+++ open-market-discovery/src/test/java/org/com/openmarket/discovery/OpenMarketDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.openmarket.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMarketDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
