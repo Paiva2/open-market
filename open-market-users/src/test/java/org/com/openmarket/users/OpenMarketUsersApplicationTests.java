f7388807475f0385fd223a2a9857bc983fef8ce9
paiva2 <joaovitor.paiva145@hotmail.com>
2024-11-20 14:54:06 -0300

    [CHORE] init users service

---
+++ open-market-users/src/test/java/org/com/openmarket/users/OpenMarketUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.openmarket.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMarketUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
