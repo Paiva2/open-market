2dd2c5e2c0cac3067b986d52049b7d06f465db47
paiva2 <joaovitor.paiva145@hotmail.com>
2024-11-29 00:10:43 -0300

    [CHORE] create Products service

---
+++ open-market-products/src/test/java/org/com/openmarket/products/OpenMarketProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.openmarket.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMarketProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
